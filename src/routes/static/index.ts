981b84d501665990d32bca887c9af617e031cc5b
Jorge De Jesus Cardoso <tst.jorgecardoso@gmail.com>
2024-10-15 16:26:41 -0300

    update dir

--- src/routes/static/index.ts
+++
@@ -1,9 +0,0 @@
-import express,{ Router } from 'express';
-import path from 'path';
-
-const staticRoute = Router();
-
-staticRoute.use('/view/candidate/img', express.static(path.join(__dirname, '..', 'uploads/photo')));
-staticRoute.use('/view/candidate/curriculum', express.static(path.join(__dirname, '..', 'uploads/curriculum')));
-
-export default staticRoute;
